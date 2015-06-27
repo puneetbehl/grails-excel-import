c933b6368e693e05f74ca719aa4ff5470905ebf0
Puneet Behl <puneet.behl@intelligrape.com>
2015-06-27 23:45:25 +0530

    added joda-time Grails3 version in dependency

--- src/main/groovy/grails/plugins/excelimport/ExcelImportGrailsPlugin.groovy
+++ src/main/groovy/grails/plugins/excelimport/ExcelImportGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class ExcelImportGrailsPlugin extends Plugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "3.0.1 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [:]
+    def dependsOn = ["joda-time":"2.0 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
